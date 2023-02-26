2bc36983a5628b5f3438eec9e9de8ed7aa1e44e1
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2023-02-26 10:30:17 +0100

    create the gateway service

---
+++ gateway/src/main/java/com/itnexusglobal/gateway/GatewayApplication.java
@@ -0,0 +1,33 @@
+package com.itnexusglobal.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+//	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return  builder.routes()
+				.route(r->r.path("/persons/**").uri("lb://PERSON-SERVICE"))
+				.route(r->r.path("/api/roles/**").uri("lb://PERSON-SERVICE"))
+				.route(r->r.path("/api/profiles/**").uri("lb://PROFILE-SERVICE"))
+				.build();
+	}
+	@Bean
+	public DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+	}
+
+
+}
